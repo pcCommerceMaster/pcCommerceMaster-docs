a0964a30d8715117cbe2166d4788637d54817e91
ljh <leejunghyun1019@gmail.com>
2026-02-20 11:05:14 +0900

    feat: 공통 응답 및 예외 처리 구현

---
+++ src/main/java/com/pc_commerce_master/PcCommerceMasterApplication.java
@@ -0,0 +1,13 @@
+package com.pc_commerce_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcCommerceMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcCommerceMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pc_commerce_master/PcCommerceMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc_commerce_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcCommerceMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
